63bb694d7853da0c7e37ddc805981e7d8d26ccab
jimcheung <hi.jinhu.zhang@gmail.com>
2024-04-21 15:04:57 +0800

    fix

--- power_on.rb
+++ power_on.rb
@@ -6,9 +6,9 @@ require "wol"
 
 options = {}
 OptionParser.new do |opt|
-  opt.on("-m", "--mac MAC", "mac address") { |o| options[:mac] = o || ENV['MAC'] }
-  opt.on("-a", "--address ADDRESS", "IP or domain") { |o| options[:address] = o || ENV['ADDRESS']}
-  opt.on("-p", "--port PORT", "port") { |o| options[:port] = o || ENV['PORT'] }
+  opt.on("-m", "--mac MAC", "mac address") { |o| options[:mac] = o }
+  opt.on("-a", "--address ADDRESS", "IP or domain") { |o| options[:address] = o }
+  opt.on("-p", "--port PORT", "port") { |o| options[:port] = o }
 end.parse!
 
 result = Wol::WakeOnLan.new(options).wake
